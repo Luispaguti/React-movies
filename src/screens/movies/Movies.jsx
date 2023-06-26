9ce51f80e14328df38f3c3515e20a4afc85c7276
Luispaguti <l.palaciosgutierrez@gmail.com>
2023-06-26 19:25:59 +0200

    handleerror

--- src/screens/movies/Movies.jsx
+++ src/screens/movies/Movies.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import moviesData from '../../data/sample.json';
+import pop from '../../../src/popcorn.jpg'
 
 function Movies() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const filteredMovies = moviesData.entries.filter((movie) => movie.programType === 'movie');
+    const filteredMovies = moviesData.entries.filter((movie) => movie.programType === 'movie' && movie.releaseYear >= 2010);
     setMovies(filteredMovies.slice(0, 20));
   }, []);
 
+  console.log(moviesData.entries[1].releaseYear)
+
+  const handleImageError = (event) => {
+    event.target.src = pop;
+    event.target.style.objectFit = 'cover';
+    event.target.style.width = '100%';
+    event.target.style.height = '100%';
+  };
+
   return (
-    <div className="container py-4">
+    <div className="container">
       <div className="d-flex align-content-start flex-wrap">
         {movies.map((movie) => (
-          <div className="card mx-2" style={{ width: '8rem', height: '8rem' }} key={movie.title}>
-            <img src={movie.images['Poster Art'].url} className="card-img-top" alt="..." />
-            <div className="card-body"></div>
-            <p className="card-title">{movie.title}</p>
-          </div>
-        ))}
+          <div className="card mx-2 my-5" style={{ width: '8rem', height: '12rem' }} key={movie.title}>
+            <img src={movie.images['Poster Art'].url} 
+            className="card-img"
+              alt="..."
+              onError={handleImageError}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+               />
+               <div className='card-body'>
+               <p className="card-title" style={{ marginTop: '0.2rem' }}>{movie.title}</p>
+               </div>
+            
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-}
-
+    );
+  }
 export default Movies;
--- src/screens/series/Series.jsx
+++ src/screens/series/Series.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import seriesData from '../../data/sample.json';
+import pop from '../../../src/popcorn.jpg'
 
 function Series() {
   const [series, setSeries] = useState([]);
 
   useEffect(() => {
-    const filteredSeries = seriesData.entries.filter((serie) => serie.programType === 'series');
+    const filteredSeries = seriesData.entries.filter((serie) => serie.programType === 'series' && serie.releaseYear >= 2010);
     setSeries(filteredSeries.slice(0, 20));
   }, []);
+
+  const handleImageError = (event) => {
+    event.target.src = pop;
+    event.target.style.objectFit = 'cover';
+    event.target.style.width = '100%';
+    event.target.style.height = '100%';
+  };
 
   return (
     <div className="container py-4">
       <div className="d-flex align-content-start flex-wrap">
         {series.map((serie) => (
-          <div className="card mx-2" style={{ width: '8rem', height: '8rem' }} key={serie.title}>
-            <img src={serie.images['Poster Art'].url} className="card-img-top" alt="..." />
-            <div className="card-body"></div>
-            <p className="card-title">{serie.title}</p>
+          <div className="card mx-2 my-5" style={{ width: '8rem', height: '12rem' }} key={serie.title}>
+            <img 
+            src={serie.images['Poster Art'].url}
+             className="card-img"
+              alt="..."
+              onError={handleImageError}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+               />
+               <div className='card-body'>
+               <p className="card-title" style={{ marginTop: '0.2rem' }}>{serie.title}</p>
+               </div>
+            
           </div>
         ))}
       </div>
